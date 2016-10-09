873dbe2c4604c3b38f6d7408d186d420aa0164c7
Nehil Jain <jain.nehil@gmail.com>
2016-10-09 13:17:07 -0300

    test case with indentation problem

--- tests/test_report_summary.py
+++ tests/test_report_summary.py
@@ -8,6 +8,19 @@ def check_content_type(headers):
 
 
 def test_reporting_article_summary():
-    rv = test_app.get('/reporting/articles/summary')
+	d = dict(
+				report_request=dict(
+			    	user_id="selvam4@test.com",
+			    	date_ranges=[
+			        	dict(start_date="2015-06-15",end_date="2015-06-25")
+			    	]
+			    ) 
+			)
+	print(d)
+    rv = test_app.post('/reporting/articles/summary', data=d)
     check_content_type(rv.headers)
     eq_(rv.status_code, 200)
+
+    resp = json.loads(rv.data)
+    eq_(resp['user_id'], "selvam4@test.com")
+    assert len(resp['report']) > 0 
